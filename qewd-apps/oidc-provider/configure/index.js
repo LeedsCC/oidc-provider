31c2dc23453e4f8cb30c30d3d4b0320eb7548244
robtweed <rtweed@mgateway.com>
2019-03-14 16:50:09 +0000

    Initial Commit

---
+++ qewd-apps/oidc-provider/configure/index.js
@@ -0,0 +1,99 @@
+/*
+
+ ----------------------------------------------------------------------------
+ | oidc-provider: OIDC Provider QEWD-Up MicroService                        |
+ |                                                                          |
+ | Copyright (c) 2019 M/Gateway Developments Ltd,                           |
+ | Redhill, Surrey UK.                                                      |
+ | All rights reserved.                                                     |
+ |                                                                          |
+ | http://www.mgateway.com                                                  |
+ | Email: rtweed@mgateway.com                                               |
+ |                                                                          |
+ |                                                                          |
+ | Licensed under the Apache License, Version 2.0 (the "License");          |
+ | you may not use this file except in compliance with the License.         |
+ | You may obtain a copy of the License at                                  |
+ |                                                                          |
+ |     http://www.apache.org/licenses/LICENSE-2.0                           |
+ |                                                                          |
+ | Unless required by applicable law or agreed to in writing, software      |
+ | distributed under the License is distributed on an "AS IS" BASIS,        |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ | See the License for the specific language governing permissions and      |
+ |  limitations under the License.                                          |
+ ----------------------------------------------------------------------------
+
+  14 March 2019
+
+*/
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(messageObj, session, send, finished) {
+
+  // Configure with initial data if present
+
+  var oidcDoc = this.db.use(this.oidc.documentName);
+  var data;
+
+  try {
+    var data = require('/opt/qewd/mapped/configuration/data.json');
+  }
+  catch(err) {
+  }
+
+  var id;
+  var now = new Date().toISOString();
+  var salt = bcrypt.genSaltSync(10);
+  var password;
+
+  if (!oidcDoc.$('Access').exists && data) {
+    if (data.Access) {
+      var accessDoc = oidcDoc.$('Access');
+      data.Access.forEach(function(record) {
+        id = accessDoc.$('next_id').increment();
+        record.id = id;
+        password = record.password || 'password';
+        record.password = bcrypt.hashSync(password, salt);
+        record.verified = true;
+        record.createdBy = 1;
+        record.createdAt = now;
+        record.modifiedBy = 1;
+        record.modifiedAt = now;
+        accessDoc.$(['by_id', id]).setDocument(record);
+      });
+    }
+    if (data.Claims) {
+      var claimsDoc = oidcDoc.$('Claims');
+      data.Claims.forEach(function(record) {
+        id = claimsDoc.$('next_id').increment();
+        claimsDoc.$(['by_id', id]).setDocument(record);
+      });
+    }
+    if (data.Clients) {
+      var clientsDoc = oidcDoc.$('Clients');
+      data.Clients.forEach(function(record) {
+        id = clientsDoc.$('next_id').increment();
+        clientsDoc.$(['by_id', id]).setDocument(record);
+      });
+    }
+    if (data.Users) {
+      var usersDoc = oidcDoc.$('Users');
+      data.Users.forEach(function(record) {
+        id = usersDoc.$('next_id').increment();
+        password = record.password || 'password';
+        record.password = bcrypt.hashSync(password, salt);
+        record.verified = true;
+        record.hcp_id = 1;
+        record.createdBy = 1;
+        record.createdAt = now;
+        record.updatedBy = 1;
+        record.updatedAt = now;
+        usersDoc.$(['by_id', id]).setDocument(record);
+      });
+    }
+  }
+
+  finished({ok: true});
+};
---
+++ qewd-apps/oidc-provider/getUser/index.js
@@ -0,0 +1,74 @@
+/*
+
+ ----------------------------------------------------------------------------
+ | oidc-provider: OIDC Provider QEWD-Up MicroService                        |
+ |                                                                          |
+ | Copyright (c) 2019 M/Gateway Developments Ltd,                           |
+ | Redhill, Surrey UK.                                                      |
+ | All rights reserved.                                                     |
+ |                                                                          |
+ | http://www.mgateway.com                                                  |
+ | Email: rtweed@mgateway.com                                               |
+ |                                                                          |
+ |                                                                          |
+ | Licensed under the Apache License, Version 2.0 (the "License");          |
+ | you may not use this file except in compliance with the License.         |
+ | You may obtain a copy of the License at                                  |
+ |                                                                          |
+ |     http://www.apache.org/licenses/LICENSE-2.0                           |
+ |                                                                          |
+ | Unless required by applicable law or agreed to in writing, software      |
+ | distributed under the License is distributed on an "AS IS" BASIS,        |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ | See the License for the specific language governing permissions and      |
+ |  limitations under the License.                                          |
+ ----------------------------------------------------------------------------
+
+  12 March 2019
+
+*/
+
+module.exports = function(messageObj, session, send, finished) {
+  var email;
+  if (messageObj.params) email = messageObj.params.id;
+  if (!email || email === '') {
+    return finished({error: 'Missing or empty id'});
+  }
+
+  var usersDoc = this.db.use(this.oidc.documentName, 'Users');
+  var emailIndex = usersDoc.$(['by_email', email]);
+  if (!emailIndex.exists) {
+    return finished({error: 'No such user'});
+  }
+  var id = emailIndex.value;
+  var userDoc = usersDoc.$(['by_id', id]);
+
+  if (userDoc.exists) {
+    var data = {
+      sub: id
+    }; 
+    var scope = messageObj.params.scope;
+    var claimsDoc = this.db.use(this.oidc.documentName, 'Claims');
+    var claimId = claimsDoc.$(['by_name', scope]).value;
+    if (claimId !== '') {
+      var _this = this;
+      var fields = claimsDoc.$(['by_id', claimId, 'fields']).getDocument(true);
+      fields.forEach(function(fieldName) {
+        if (fieldName !== 'vouchedBy') {
+          data[fieldName] = userDoc.$(fieldName).value;
+        }
+        else {
+          var hcp_id = userDoc.$('hcp_id').value;
+          var accessDoc = _this.db.use(_this.oidc.documentName, 'Access', 'by_id', hcp_id);
+          var owner = accessDoc.$('name').value;
+          if (owner === '') owner = 'Not Known';
+          data[fieldName] = owner;              
+        }
+      });
+    }
+    finished(data);
+  }
+  else {
+    finished({error: 'No such User'});
+  }
+};
